7029da353c9dbfb609508b02a4d494c54a468e13
Olivier Desnoë <olivier@albatross-networks.com>
2022-01-11 17:16:44 +0100

    adding poetry virtual env 🎈🎈

--- scripts/netbox-psql/restore.sh
+++ scripts/netbox-psql/restore.sh
@@ -5,5 +5,5 @@ SQL_DUMP=$1
 PG_URL=$2
 PSQL=$(which psql)
 
-$PSQL -1 -f "$SQL_CLEAR" "$PG_URL"
-$PSQL -1 -f "$SQL_DUMP" "$PG_URL"
+"$PSQL" -1 -f "$SQL_CLEAR" "$PG_URL"
+"$PSQL" -1 -f "$SQL_DUMP" "$PG_URL"
